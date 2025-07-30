f133878cae232f610aa8b1da8b215c5ac887ee89
Vinicius Costa <costavinicius692@gmail.com>
2025-07-30 16:47:37 -0300

    criando backend

--- infra/main.tf
+++ infra/main.tf
@@ -1,32 +1,47 @@
+# VPC
 module "vpc" {
-  source = "git::https://github.com/Vinicius-DevOps/terraform-aws-modules.git//modules/vpc?ref=main"
-
-  environment          = var.environment
+  source               = "git::https://github.com/Vinicius-DevOps/terraform-aws-modules.git//modules/vpc?ref=main"
   vpc_cidr_block       = var.vpc_cidr_block
-  public_subnet_cidrs  = [var.public_subnet_cidrs[0], var.public_subnet_cidrs[1]]
-  private_subnet_cidrs = [var.private_subnet_cidrs[0], var.private_subnet_cidrs[1]]
+  environment          = var.environment
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
 }
 
+# SG
 module "sg" {
-  source = "git::https://github.com/Vinicius-DevOps/terraform-aws-modules.git//modules/security-group?ref=main"
-  vpc_id = module.vpc.vpc_id
+  source      = "git::https://github.com/Vinicius-DevOps/terraform-aws-modules.git//modules/sg?ref=main"
+  environment = var.environment
+  port_number = var.port_number
+  vpc_id      = module.vpc.vpc_id
 }
 
-module "alb" {
-  source              = "git::https://github.com/Vinicius-DevOps/terraform-aws-modules.git//modules/alb?ref=main"
-  vpc_id              = module.vpc.vpc_id
-  security_groups_ids = [module.sg.security_group_id]
-  subnets_ids         = [module.vpc.public_subnet_ids[0], module.vpc.public_subnet_ids[1]]
-
-}
-
+# ASG
 module "asg" {
-  source                 = "git::https://github.com/Vinicius-DevOps/terraform-aws-modules.git//modules/autoscaling-group?ref=main"
-  name                   = var.environment
-  ami_id                 = var.ami_id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.sg.security_group_id]
-  subnet_ids             = [module.vpc.private_subnet_ids[0], module.vpc.private_subnet_ids[1]]
-  target_group_arns      = [module.alb.target_group_arn]
-  environment            = var.environment
+  source             = "git::https://github.com/Vinicius-DevOps/terraform-aws-modules.git//modules/asg?ref=main"
+  environment        = var.environment
+  ami_id             = var.ami_id
+  instance_type      = var.instance_type
+  security_group_id  = module.sg.security_group_id
+  key_name           = var.key_name
+  user_data          = var.user_data
+  private_subnet_ids = module.vpc.private_subnet_ids
+  min_size           = var.min_size
+  max_size           = var.max_size
+  desired_capacity   = var.desired_capacity
+  target_group_arn   = module.alb.target_group_arn
+}
+
+# ALB
+module "alb" {
+  source                = "git::https://github.com/Vinicius-DevOps/terraform-aws-modules.git//modules/alb?ref=main"
+  environment           = var.environment
+  security_group_id     = module.sg.security_group_id
+  subnet_public_id      = module.vpc.public_subnet_ids
+  target_group_port     = var.target_group_port
+  target_group_protocol = var.target_group_protocol
+  vpc_id                = module.vpc.vpc_id
+  health_check_path     = var.health_check_path
+  health_check_port     = var.health_check_port
+  listener_port         = var.listener_port
+  listener_protocol     = var.listener_protocol
 }
